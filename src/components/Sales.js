7d980fb7b9849e5e84ed3b07c20d9687bbe2012c
Sebastian Solar <presercomp@presercomp.local>
2022-07-18 20:50:42 -0400

    añade funcionalidad de resumen

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -16,7 +16,15 @@ class Sales extends React.Component{
             stock: 0,
             MDPrice: 0,
             MDPercentge: 0,
-            active: true
+            active: true,
+            total: 0
+        },
+        cart: [],
+        resumen : {
+            neto: 0,
+            taxs: 0,
+            otherTaxs: 0,
+            final: 0,
         }
     }
 
@@ -27,6 +35,85 @@ class Sales extends React.Component{
             product: product
         });
     }
+
+    calculateTotal = () => {
+        let product = this.state.product;
+        let qty = document.getElementById("qty").value;        
+        let discount = document.getElementById("discount").value;
+        let isPercentage = discount.indexOf("%") > -1;
+        if(isPercentage){
+            discount = discount.replace("%", "") / 100;
+            product.total = (product.price * qty)-(product.price * qty * discount);
+        } else {
+            product.total = (product.price * qty)-discount;
+        }        
+        this.setState({
+            product: product
+        });
+    }
+    
+
+    addToCart = () => {
+        let product = this.state.product;
+        let errors = "";
+        let qty = document.getElementById("qty").value;        
+        let discount = document.getElementById("discount").value;
+        let isPercentage = false;
+        if(discount.length > 0){
+            isPercentage = discount.indexOf("%") > -1;            
+        } else {
+            discount = 0;            
+        }
+        errors += product.name === "" ? "Seleccione un producto\n" : "";
+        errors += qty === "" ? "Se necesita la cantidad\n" : "";
+        errors += isPercentage && parseInt(discount.replace("%","")) > product.MDPercentge ? "No puede descontar más del "+product.MDPercentge+"%\n" : "";
+        errors += !isPercentage && parseInt(discount) > product.MDPrice ? "No puede descontar más de $"+product.MDPrice+".\n" : "";
+        errors += qty > product.stock ? "No puede vender más de "+product.stock+" productos\n" : "";
+        if(errors.length > 0){
+            alert(errors);
+        } else {
+            let cartRow = {id: product.id+'-'+qty, productId: product.id, productName: product.name, qty: qty, price: product.price, discount: discount, total: product.total };
+            this.setState({
+                cart: [...this.state.cart, cartRow]
+            });
+            this.getResumen();   
+        }
+        
+    }
+
+    removeCartItem = (item) => {
+        let cart = this.state.cart;
+        let index = cart.indexOf(item);
+        cart.splice(index, 1);
+        this.setState({
+            cart: cart
+        });
+        this.getResumen();
+    }
+
+    getResumen = () => {
+        setTimeout(() => {        
+            let cart = this.state.cart;
+            var res = {
+                neto: 0,
+                taxs: 0,
+                otherTaxs: 0,
+                final: 0,
+            }
+            cart.forEach(item => { 
+                res.neto += item.total;            
+            });
+            res.taxs = res.neto * 0.19;
+            res.otherTaxs = 0;
+            res.final = res.neto + res.taxs + res.otherTaxs;
+            this.setState({
+                resumen: res
+            });
+        }, 200)
+    }
+    
+
+    
 
     componentDidMount() {
         const requestOptions = {
@@ -109,41 +196,41 @@ class Sales extends React.Component{
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control">U.M.V.</label>
-                                        <input className="form-control" type="text" name="msv" id="msv" readOnly="readonly" value={this.state.product.MSU} />
+                                        <input className="form-control" type="text" name="msv" id="msv" readOnly={true} value={this.state.product.MSU}/>
                                     </div>
                                 </div>
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control" >Stock</label>
-                                        <input className="form-control" type="text" name="stock" id="stock" readOnly="readonly" value={this.state.product.stock} />
+                                        <input className="form-control" type="text" name="stock" id="stock" readOnly={true} value={this.state.product.stock}/>
                                     </div>
                                 </div>
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control" >Cantidad</label>
-                                        <input className="form-control" type="number" name="qty" id="qty" />
+                                        <input className="form-control" type="number" name="qty" id="qty" onChange={this.calculateTotal}/>
                                     </div>
                                 </div>
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control">Unitario</label>
-                                        <input className="form-control" type="text" name="unitary" id="unitary" value={this.state.product.price} />
+                                        <input className="form-control" type="text" name="unitary" id="unitary" readOnly={true}  value={this.state.product.price} />
                                     </div>
                                 </div>
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control" >Descuento</label>
-                                        <input className="form-control" type="number" name="discount" id="discount" />
+                                        <input className="form-control" type="text" name="discount" id="discount" onChange={this.calculateTotal} />
                                     </div>
                                 </div>
                                 <div className="col-4">
                                     <div className="form-group">
                                         <label className="label-control" >Total</label>
-                                        <input className="form-control" type="number" name="total" id="total" />
+                                        <input className="form-control" type="text" name="total" id="total" readOnly={true}  value={this.state.product.total} />
                                     </div>
                                 </div>
                                 <div className="col-12">
-                                    <button className="btn btn-primary btn-block">
+                                    <button className="btn btn-primary btn-block" onClick={this.addToCart}>
                                         <i className="fas fa-cart-plus" />Agregar
                                     </button>
                                 </div>
@@ -160,23 +247,37 @@ class Sales extends React.Component{
                                                 <th>Accion</th>
                                             </tr>
                                         </thead>
+                                        <tbody>
+                                        {this.state.cart.map(cl => (
+                                                <tr key={cl.id}>
+                                                    <td>{cl.productName}</td>
+                                                    <td>{cl.qty}</td>
+                                                    <td>{cl.total}</td>
+                                                    <td>
+                                                        <button className="btn btn-danger" onClick={this.removeCartItem.bind(cl)}>
+                                                            <i className="fas fa-trash" />
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
                                     </table>
                                 </div>
                                 <div className="col-6">
                                     <label className="control-label">Neto</label>
-                                    <input className="form-control" type="text" name="neto" id="neto" readOnly="readonly" value="0" />
+                                    <input className="form-control" type="text" name="neto" id="neto" readOnly={true} value={this.state.resumen.neto} />
                                 </div>
                                 <div className="col-6">
                                     <label className="control-label">IVA</label>
-                                    <input className="form-control" type="text" name="tax" id="tax" readOnly="readonly" value="0" />
+                                    <input className="form-control" type="text" name="tax" id="tax" readOnly={true} value={this.state.resumen.taxs} />
                                 </div>
                                 <div className="col-6">
                                     <label className="control-label">Otros Impuestos</label>
-                                    <input className="form-control" type="text" name="otherTaxs" id="otherTaxs" readOnly="readonly" value="0" />
+                                    <input className="form-control" type="text" name="otherTaxs" id="otherTaxs" readOnly={true} value={this.state.resumen.otherTaxs} />
                                 </div>
                                 <div className="col-6">
                                     <label className="control-label">Total</label>
-                                    <input className="form-control" type="text" name="final" id="final" readOnly="readonly" value="0" />
+                                    <input className="form-control" type="text" name="final" id="final" readOnly={true} value={this.state.resumen.final} />
                                 </div>
                                 <div className="col-12">
                                     <hr />
